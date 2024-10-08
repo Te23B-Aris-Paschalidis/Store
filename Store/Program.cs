a3e663b4af23db0a9084a25c4bb714e10de78883
Aris Paschalidis <aris.paschalidis@elev.ga.ntig.se>
2024-10-08 13:22:04 +0200

    wdasgaeag

--- Store/Program.cs
+++ Store/Program.cs
@@ -1,37 +1,61 @@
-﻿int i = 0;
-while (i < 10)
+﻿int kronor = 100;
+
+Console.WriteLine($"Du har {kronor} kronor att spendera\n");
+Console.WriteLine("Vad vill du köpa?");
+Console.WriteLine("1. Keps 20kr");
+Console.WriteLine("2. Tröja 30kr");
+Console.WriteLine("3. Jeans 50kr");
+
+string val = Console.ReadLine();
+
+Console.WriteLine("Hur många?");
+string antal = Console.ReadLine();
+
+if (val == "1")
 {
-Console.WriteLine("hej");
-i++;
+
 }
-
-for (int j=0; j < 10; j++)
-{
-    Console.WriteLine("hej");
-}
-
-
-
-bool success = false;
-int ageNum = 0;
-
-while (success != true)
-{
-    Console.WriteLine("Ålder:");
-    string age = Console.ReadLine();
-
-
-    success = int.TryParse(age, out ageNum);
-
-    if (success == false)
-    {
-        Console.WriteLine("Skriv en siffra, dumhuvve");
-    }
-}
-
-if (ageNum >= 18)
-{
-    Console.WriteLine("Välkommen!");
-}
-
 Console.ReadLine();
+
+
+
+
+
+
+// int i = 0;wqdwdsa
+// while (i < 10)
+// {
+// Console.WriteLine("hej");
+// i++;
+// }
+
+// for (int j=0; j < 10; j++)
+// {
+//     Console.WriteLine("hej");
+// }
+
+
+
+// bool success = false;
+// int ageNum = 0;
+
+// while (success != true)
+// {
+//     Console.WriteLine("Ålder:");
+//     string age = Console.ReadLine();
+
+
+//     success = int.TryParse(age, out ageNum);
+
+//     if (success == false)
+//     {
+//         Console.WriteLine("Skriv en siffra, dumhuvve");
+//     }
+// }
+
+// if (ageNum >= 18)
+// {
+//     Console.WriteLine("Välkommen!");
+// }
+
+// Console.ReadLine();
--- Store/obj/Debug/net8.0/Store.AssemblyInfo.cs
+++ Store/obj/Debug/net8.0/Store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f4d9e58f7ddcf4f13e97dd17367205bddca8ff2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
